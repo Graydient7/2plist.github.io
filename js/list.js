70580364e5c7f4f75377dabc904a79e87e0977cb
2plist <121057722+2plist@users.noreply.github.com>
2022-12-31 23:54:27 -0500

    update with list data

--- js/list.js
+++ js/list.js
@@ -1,58 +1,550 @@
 const d = {
-	"list": [
-		{
-			// The position of a level on the list. The actual webpage will start at 0
-			// and not skip any numbers, but these keys are just used for sorting the
-			// list, so you can do -2, 4, and 8, and the demonlist will sort them as 1,
-			// 2, and 3.
-			"key": 1,
-
-			// Was the level hacked or not? If true, the page will not display a video,
-			// and "HACKED" will be shown next to the level name.
-			"hacked": false,
-
-			// The video on the homepage containing the verification (or, in the case of
-			// this demonlist, the first one-player completion. It can be a full link
-			// (https://www.youtube.com/watch?v=dQw4w9WgXcQ), a video ID (dQw4w9WgXcQ),
-			// or a shortened URL (https://youtu.be/dQw4w9WgXcQ). If this is null or "",
-			// the video will not display on the demonlist. (This is also the case if
-			// hacked is set to true.)
-			"verificationVid": "dQw4w9WgXcQ",
-
-			// The name of the level.
-			"name": "Level Name",
-
-			// The ID of the level.
-			"id": "12345678",
-
-			// The creator(s) of the level.
-			"author": "DigitalZero and more",
-
-			// The description of the level.
-			"desc": "Bagn Gagn",
-
-			// Whether the level requires a handcam or not.
+	"list": [{
+			"key": "1",
+			"id": "64100176",
+			"name": "\ud83c\udf1f Tenkai",
+			"percentToQualify": "68",
 			"handcam": true,
-
-			// The percentage needed for a record to qualify. This doesn't have any
-			// functional use, so it may be a string and thus hold any arbitrary value.
-			"percentToQualify": "55",
-
-			// A list of records. An example record is provided.
+			"hacked": false,
+			"verificationVid": "",
 			"vids": [
-				{
-					// The percentage of the record. This does have a practical usage, and thus
-					// it must not be a string and therefore it must not be in quotes.
-					"percent": 56,
 
-					// A link to the record. MUST be an HTTPS link.
-					"link": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-
-					// The record creator.
-					"user": "Graydient7",
-				},
 			],
+			"author": "HidekiX",
+			"desc": "thank you for playing"
 		},
+		{
+			"key": "2",
+			"id": "71692057",
+			"name": "Based Gang",
+			"percentToQualify": "54",
+			"handcam": true,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "Alphirox",
+			"desc": "2-Player Based Extreme Demon Verified by GiggsRH and Koopazu. Layout by Meekah and Arts by ItsZPlayer."
+		},
+		{
+			"key": "3",
+			"id": "56916170",
+			"name": "Bang Gang",
+			"percentToQualify": "59",
+			"handcam": true,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "Danzmen",
+			"desc": "2-player Slap Squad spinoff. Verified in Npesta's house."
+		},
+		{
+			"key": "4",
+			"id": "85231431",
+			"name": "Gentlemens Clique",
+			"percentToQualify": "61",
+			"handcam": true,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "Alphirox",
+			"desc": "A little Based Gang spinoff verified by Dnuoz and NathanYAY during their meetup. The layout was also made by the two, go check them out :)"
+		},
+		{
+			"key": "5",
+			"id": "38142097",
+			"name": "Colorful Corruption",
+			"percentToQualify": "60",
+			"handcam": true,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "albinomaster",
+			"desc": "New 2 player insane demon. Verified by albinomaster and mousey333. Video and pass on YouTube. plz sub xd"
+		},
+		{
+			"key": "6",
+			"id": "24234025",
+			"name": "\ud83c\udf1f Epic Multiplayer",
+			"percentToQualify": "38",
+			"handcam": true,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "NachoLag",
+			"desc": "wow that was insane :,v it took us like 10000 attempts to verify it, by me and Matowaar, pass on Yt, rate Demon :3 nerfed a lot. Updated"
+		},
+		{
+			"key": "7",
+			"id": "60660086",
+			"name": "Multition",
+			"percentToQualify": "68",
+			"handcam": true,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "Mulpan",
+			"desc": "Verified by Mul + Tition [Real 2-player Demon]"
+		},
+		{
+			"key": "8",
+			"id": "69866281",
+			"name": "Arcane16",
+			"percentToQualify": "74",
+			"handcam": true,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "LCynox",
+			"desc": "verified with TanTrum1337 in 3200 attempts. Toilet paper"
+		},
+		{
+			"key": "9",
+			"id": "26157409",
+			"name": "Elite Z Rebirth",
+			"percentToQualify": "64",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "KrMaL",
+			"desc": "Updated version of elite z by PLAY 4075365, redesigned by AirSwipe and me."
+		},
+		{
+			"key": "10",
+			"id": "40902405",
+			"name": "CholeriX",
+			"percentToQualify": "50",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "shaggy23",
+			"desc": "My Final Megacollab, Extreme Demon, this was made in 2.0 update :) Verified by GK Allium (Check his YT). Video on my Channel ;)"
+		},
+		{
+			"key": "11",
+			"id": "57359114",
+			"name": "Master Dual",
+			"percentToQualify": "100",
+			"handcam": true,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "Zylenox",
+			"desc": "both teacher"
+		},
+		{
+			"key": "12",
+			"id": "23298409",
+			"name": "Duelo Maestro",
+			"percentToQualify": "100",
+			"handcam": true,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "Nacho21",
+			"desc": "El duelo de tus 2 manos o de 2 verdaderos maestros. El modo de 2 jugadores esta roto, asi que reinicien el nivel de vez en cuando..."
+		},
+		{
+			"key": "13",
+			"id": "86284711",
+			"name": "Hex Masters Manor",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "Custi",
+			"desc": "HEX MASTER Remake. Good Luck!"
+		},
+		{
+			"key": "14",
+			"id": "43001774",
+			"name": "ApocalypseAfterParty",
+			"percentToQualify": "100",
+			"handcam": true,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "WHErwin",
+			"desc": "What would you do if you were the last person on earth? Dedicated to Edicts."
+		},
+		{
+			"key": "15",
+			"id": "62717112",
+			"name": "Cosmic Dolphin",
+			"percentToQualify": "100",
+			"handcam": true,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "xDon",
+			"desc": ""
+		},
+		{
+			"key": "16",
+			"id": "68177479",
+			"name": "Funkin ",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "Bluskys",
+			"desc": ""
+		},
+		{
+			"key": "17",
+			"id": "64405105",
+			"name": "HEX MASTER",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "Custi",
+			"desc": ""
+		},
+		{
+			"key": "18",
+			"id": "71850733",
+			"name": "Flappy Dynamix II",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "RadiationV2",
+			"desc": ""
+		},
+		{
+			"key": "19",
+			"id": "5289002",
+			"name": "\ud83c\udf1f Leviathan",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "KaptainCPU",
+			"desc": ""
+		},
+		{
+			"key": "20",
+			"id": "61417841",
+			"name": "Halibut Cannon",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "Custi",
+			"desc": ""
+		},
+		{
+			"key": "21",
+			"id": "30861262",
+			"name": "Horntail Cave II",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "KeiAs",
+			"desc": ""
+		},
+		{
+			"key": "22",
+			"id": "72224038",
+			"name": "Temple Profanators",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "BAARBAS",
+			"desc": ""
+		},
+		{
+			"key": "23",
+			"id": "15699542",
+			"name": "Absolute Meltdown",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "Pan",
+			"desc": ""
+		},
+		{
+			"key": "24",
+			"id": "7005127",
+			"name": "Horntail Cave",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "KeiAs",
+			"desc": ""
+		},
+		{
+			"key": "25",
+			"id": "9962546",
+			"name": "10",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "T3mpl4te",
+			"desc": ""
+		},
+		{
+			"key": "26",
+			"id": "47994830",
+			"name": "Star Power",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "CarlosArt16",
+			"desc": ""
+		},
+		{
+			"key": "27",
+			"id": "13102433",
+			"name": "Ebony",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "Ulasp",
+			"desc": ""
+		},
+		{
+			"key": "28",
+			"id": "10978740",
+			"name": "Choleric",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "ZillaGong",
+			"desc": ""
+		},
+		{
+			"key": "29",
+			"id": "32612656",
+			"name": "HyperDimension",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "ChaSe",
+			"desc": ""
+		},
+		{
+			"key": "30",
+			"id": "77592888",
+			"name": "YOQUIERO",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "Vrymer",
+			"desc": ""
+		},
+		{
+			"key": "31",
+			"id": "8969004",
+			"name": "Graveyard",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "Splenetic",
+			"desc": ""
+		},
+		{
+			"key": "32",
+			"id": "4891112",
+			"name": "Tiny Express",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "softable",
+			"desc": ""
+		},
+		{
+			"key": "33",
+			"id": "62258138",
+			"name": "Phosbat Cave",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "Downstop",
+			"desc": ""
+		},
+		{
+			"key": "34",
+			"id": "2080527",
+			"name": "Invaders",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "DJ titanium",
+			"desc": ""
+		},
+		{
+			"key": "35",
+			"id": "58364131",
+			"name": "Fracture Breaker",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "TroxxP1",
+			"desc": ""
+		},
+		{
+			"key": "36",
+			"id": "1717018",
+			"name": "Dragon Force",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "soRakami",
+			"desc": ""
+		},
+		{
+			"key": "37",
+			"id": "52917530",
+			"name": "ARRE Robot",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "izhar",
+			"desc": ""
+		},
+		{
+			"key": "38",
+			"id": "61139219",
+			"name": "ARRE Toro",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "16lord",
+			"desc": ""
+		},
+		{
+			"key": "39",
+			"id": "61656103",
+			"name": "Claim Our Throne",
+			"percentToQualify": "100",
+			"handcam": false,
+			"hacked": false,
+			"verificationVid": "",
+			"vids": [
+
+			],
+			"author": "DYZEX",
+			"desc": ""
+		}
 	],
 
 	// The current version of the list. [x, y, z] is x.y.z.
